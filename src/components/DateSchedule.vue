2a445207167f40b2048819f1c3fa22e85d73cc7d
Masaya Kazama <miyaokamiyao@gmail.com>
2024-08-29 11:02:47 +0000

    WIP

--- src/components/DateSchedule.vue
+++ src/components/DateSchedule.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import type { DateGroup, VideoDetail } from '@/schedule'
+import type { Schedule, VideoDetail } from '@/schedule'
 import { computed, onMounted, ref } from 'vue'
 
+interface VideoWithDuration extends VideoDetail {
+  startTime: Date
+  endTime: Date
+  offset: number
+  isShorts: boolean
+}
+
 const props = defineProps<{
-  date: DateGroup
+  data: Schedule
 }>()
 
 const channels = ref<VideoWithDuration[][]>([])
@@ -12,20 +19,41 @@ const minute = 60 * 1000
 // 仮の動画の長さ
 const defaultDuration = 90 * minute
 const shortsDuration = 30 * minute
-const pixelPerHour = 150
+const hourWidth = 150
+const channelHeight = 300
 
-interface VideoWithDuration extends VideoDetail {
-  startTime: Date
-  endTime: Date
-  offset: number
-  isShorts: boolean
-}
-const currentDate = computed(() => {
-  return new Date(props.date.datetime)
+const currentDate = ref(new Date())
+
+// YYYY/mm/ddの形式で現在日付を取得
+const ymd = computed(() => {
+  return currentDate.value.toLocaleDateString('ja-JP', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  })
 })
+
+onMounted(async () => {
+  setInterval(() => {
+    currentDate.value = new Date()
+  }, 5000)
+})
+
+const dateList = computed(() => {
+  return props.data.dateGroupList.map((dateGroup) => {
+    const { displayDate, datetime } = dateGroup
+    return {
+      displayDate,
+      datetime
+    }
+  })
+})
+
+const originTime = computed(() => new Date(props.data.dateGroupList[0].datetime).getTime())
+
 const videoList = computed<VideoWithDuration[]>(() => {
-  const originTime = currentDate.value.getTime()
-  return props.date.videoList.map((video) => {
+  const wholeVideoList = props.data.dateGroupList.flatMap((dataGroup) => dataGroup.videoList)
+  return wholeVideoList.map((video) => {
     const startTime = new Date(video.datetime)
     const isShorts = false ///shorts/i.test(video.title)
     const duration = isShorts ? shortsDuration : defaultDuration
@@ -34,7 +62,7 @@ const videoList = computed<VideoWithDuration[]>(() => {
       ...video,
       startTime,
       endTime,
-      offset: (startTime.getTime() - originTime) / 1000 / 3600,
+      offset: (startTime.getTime() - originTime.value) / 1000 / 3600,
       isShorts
     }
   })
@@ -56,55 +84,65 @@ function makeChannels() {
 }
 onMounted(() => {
   makeChannels()
-  console.log('channels', channels)
 })
 </script>
 <template>
-  <div class="flex flex-col" :style="{ minWidth: `${pixelPerHour * 24}px` }">
-    <h2 class="p-4 flex bg-red-300">{{ date.displayDate }}</h2>
+  <div class="flex flex-col mt-4" :style="{ minWidth: `${hourWidth * 24 * dateList.length}px` }">
     <div class="flex flex-col relative text-xs">
       <div class="flex flex-row absolute inset-0">
-        <div v-for="hour in 24" :key="hour" class="flex-1 bg-slate-50 even:bg-white">
-          {{ hour - 1 }}
+        <div v-for="date in dateList" :key="date.datetime" class="flex-1 flex flex-row relative">
+          <div
+            :class="`absolute w-full bottom-full h-[48px] ${date.datetime.startsWith(ymd) ? 'bg-yellow-300' : ''}`"
+          >
+            <div class="text-lg">
+              {{ date.displayDate }}
+            </div>
+          </div>
+          <div v-for="hour in 24" :key="hour" class="flex-1 bg-slate-50 even:bg-white relative">
+            <div class="absolute bottom-full mb-1 ml-1">
+              {{ hour - 1 }}
+            </div>
+          </div>
         </div>
       </div>
-      <div v-for="(channel, i) in channels" :key="i" class="h-[270px] relative border-b-2">
+      <div
+        v-for="(channel, i) in channels"
+        :key="i"
+        class="relative border-t-2"
+        :style="{
+          height: `${channelHeight}px`
+        }"
+      >
         <div
           v-for="video in channel"
           :key="video.url"
-          class="absolute shadow-lg w-[240px]"
-          :class="{ isShorts: video.isShorts }"
-          :style="{ left: `${video.offset * pixelPerHour}px` }"
+          :class="`absolute bg-white shadow-lg w-[240px] rounded-[20px] overflow-hidden ${video.isLive ? 'outline outline-red-500 outline-4' : ''}`"
+          :style="{ left: `${video.offset * hourWidth}px` }"
         >
-          <div class="absolute bottom-full bg-slate-700 text-white p-1 text-sm">
+          <a :href="video.url" :title="video.title" target="_blank">
+            <img :src="video.thumbnail" class="w-full h-[135px] object-cover" loading="lazy" />
+          </a>
+          <div class="absolute bg-slate-700 top-0 text-white p-1 text-sm">
             {{ video.displayDate }}
           </div>
-
-          <div
-            :class="`bg-white rounded-[20px] overflow-hidden ${video.isLive ? 'outline outline-red-500 outline-8' : ''}`"
-          >
-            <a :href="video.url" :title="video.title" class="relative" target="_blank">
-              <img :src="video.thumbnail" class="w-full h-[135px] object-cover" />
-            </a>
-            <div class="flex flex-col p-2">
-              <div class="flex flex-row items-center gap-2">
+          <div class="flex flex-col p-2">
+            <div class="flex flex-row items-center gap-2">
+              <img
+                :src="video.talent.iconImageUrl"
+                class="rounded-full w-[36px] h-[36px]"
+                :title="video.name"
+              />
+              <h3 class="text-base">{{ video.name }}</h3>
+            </div>
+            <div>
+              <h3>{{ video.title }}</h3>
+              <div class="flex flex-row flex-wrap items-end">
                 <img
-                  :src="video.talent.iconImageUrl"
-                  class="rounded-full w-[36px] h-[36px]"
-                  :title="video.name"
+                  v-for="talent in video.collaboTalents"
+                  :key="talent.iconImageUrl"
+                  :src="talent.iconImageUrl"
+                  class="rounded-full w-[24px] h-[24px]"
                 />
-                <h3 class="text-base">{{ video.name }}</h3>
-              </div>
-              <div>
-                <h3>{{ video.title }}</h3>
-                <div class="flex flex-row flex-wrap items-end">
-                  <img
-                    v-for="talent in video.collaboTalents"
-                    :key="talent.iconImageUrl"
-                    :src="talent.iconImageUrl"
-                    class="rounded-full w-[24px] h-[24px]"
-                  />
-                </div>
               </div>
             </div>
           </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,6 @@ import { getSchedule, type Schedule } from '@/schedule'
 import { onMounted, ref } from 'vue'
 
 const data = ref<Schedule | null>(null)
-
 onMounted(async () => {
   data.value = await getSchedule()
 })
@@ -12,8 +11,8 @@ onMounted(async () => {
 
 <template>
   <main>
-    <div v-if="data" class="flex flex-row p-4">
-      <DateSchedule v-for="date in data.dateGroupList" :key="date.datetime" :date="date" />
+    <div v-if="data" class="flex flex-row p-8">
+      <DateSchedule :data="data" />
     </div>
   </main>
 </template>
