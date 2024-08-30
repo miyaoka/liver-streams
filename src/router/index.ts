fba7111ddf9e0415fbe5303d9258e04feedefb94
Masaya Kazama <miyaokamiyao@gmail.com>
2024-08-30 15:07:09 +0000

    lazy load routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import VerticalView from '@/views/VerticalView.vue'
-import HorizontalView from '@/views/HorizontalView.vue'
+const VerticalView = () => import('@/views/VerticalView.vue')
+const HorizontalView = () => import('@/views/HorizontalView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
