18485b0bbf7ca3d49858f2719b0ceba7e8dcfaa7
Masaya Kazama <miyaokamiyao@gmail.com>
2024-08-30 13:36:47 +0000

    スケジュールを自動再取得

--- src/views/VerticalView.vue
+++ src/views/VerticalView.vue
@@ -4,8 +4,14 @@ import { getSchedule, type Schedule } from '@/schedule'
 import { onMounted, ref } from 'vue'
 
 const data = ref<Schedule | null>(null)
-onMounted(async () => {
+
+async function setSchedule() {
   data.value = await getSchedule()
+}
+onMounted(async () => {
+  setSchedule()
+  // 5分毎にスケジュールを再取得
+  setInterval(setSchedule, 5 * 60 * 1000)
 })
 </script>
 
