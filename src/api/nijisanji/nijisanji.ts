74a7e1c5f10b9d0cec876540df4ff1e3c50f63d3
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-07 07:12:47 +0000

    fix type

--- src/api/nijisanji/nijisanji.ts
+++ src/api/nijisanji/nijisanji.ts
@@ -1,125 +1,126 @@
-import streamsJson from './sample1/streams.json'
-import livers from './sample1/livers.json'
+import livers from "./sample1/livers.json";
+import streamsJson from "./sample1/streams.json";
 
 export interface Stream {
-  data: Data[]
-  included: Included[]
+  data: Data[];
+  included: Included[];
 }
 export interface Data {
-  id: string
-  type: 'youtube_event'
-  attributes: Attributes
+  id: string;
+  type: "youtube_event";
+  attributes: Attributes;
   relationships: {
     youtube_channel: {
       data: {
-        id: string
-        type: 'youtube_channel'
-      }
-    }
+        id: string;
+        type: "youtube_channel";
+      };
+    };
     youtube_events_livers: {
       data: {
-        id: string
-        type: 'liver'
-      }[]
-    }
-  }
+        id: string;
+        type: "liver";
+      }[];
+    };
+  };
 }
 
 export interface Attributes {
-  title: string
-  description: string
-  url: string
-  thumbnail_url: string
-  start_at: string // '2024-09-01T23:45:00.000+09:00'
-  end_at: string
-  status: 'not_on_air' | 'on_air'
+  title: string;
+  description: string;
+  url: string;
+  thumbnail_url: string;
+  start_at: string; // '2024-09-01T23:45:00.000+09:00'
+  end_at: string;
+  status: "not_on_air" | "on_air";
 }
 
-type Included = IncludedLiver | IncludedYoutubeChannel
+type Included = IncludedLiver | IncludedYoutubeChannel;
 
 interface IncludedLiver {
-  id: string
-  type: 'liver'
+  id: string;
+  type: "liver";
   attributes: {
-    external_id: string
-  }
+    external_id: string;
+  };
   relationships: {
-    youtube_channels: {}
-  }
+    youtube_channels: {};
+  };
 }
 
 interface IncludedYoutubeChannel {
-  id: string
-  type: 'youtube_channel'
+  id: string;
+  type: "youtube_channel";
   attributes: {
-    name: string
-    thumbnail_url: string
-    main: boolean
-  }
+    name: string;
+    thumbnail_url: string;
+    main: boolean;
+  };
   relationships: {
     liver: {
       data: {
-        id: string
-        type: 'liver'
-      }
-    }
-    youtube_events: {}
-  }
+        id: string;
+        type: "liver";
+      };
+    };
+    youtube_events: {};
+  };
 }
 
 interface Liver {
-  slug: string
-  hidden: boolean
-  name: string
-  enName: string
-  images: LiverImages
-  socialLinks: LiverSocialLinks
-  siteColor: LiverSiteColor
-  id: string
-  subscriberCount: number
+  slug: string;
+  hidden: boolean;
+  name: string;
+  enName: string;
+  images: LiverImages;
+  socialLinks: LiverSocialLinks;
+  siteColor: LiverSiteColor;
+  id: string;
+  subscriberCount: number;
 }
 
 interface LiverImages {
-  fieldId: string
-  fullbody: LiverImage
-  halfbodyNew: LiverImage
-  head: LiverImage
+  fieldId: string;
+  fullbody: LiverImage;
+  halfbodyNew: LiverImage;
+  head: LiverImage;
   variation: {
-    fieldId: string
-    fullbody: LiverImage
-  }[]
-  threeDVariation: null
+    fieldId: string;
+    fullbody: LiverImage;
+  }[];
+  threeDVariation: null;
 }
 
 interface LiverImage {
-  url: string
-  width: number
-  height: number
+  url: string;
+  width: number;
+  height: number;
 }
 
 interface LiverSocialLinks {
-  fieldId: string
-  twitter: string
-  youtube: string
-  officialShop: string
+  fieldId: string;
+  twitter: string;
+  youtube: string;
+  fanclub?: string;
+  officialShop?: string;
 }
 interface LiverSiteColor {
-  id: string
-  createdAt: string // "2022-07-05T08:59:46.939Z"
-  updatedAt: string
-  publishedAt: string
-  revisedAt: string
-  name: string
-  color1: string // "#f8f8f8"
-  color2: string
+  id: string;
+  createdAt: string; // "2022-07-05T08:59:46.939Z"
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+  color1: string; // "#f8f8f8"
+  color2: string;
 }
 
 export async function getSchedule(): Promise<{ streams: Stream; livers: Liver[] }> {
   // jsonからだとunionにならないのでキャストする
-  const streams = streamsJson as unknown as Stream
+  const streams = streamsJson as unknown as Stream;
 
   return {
     streams,
-    livers
-  }
+    livers,
+  };
 }
