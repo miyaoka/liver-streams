b198d211f38ef939a1dbeab09f389e2d7c85f495
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-09 01:25:31 +0000

    reduce icon image size

--- src/api/nijisanji/nijisanji.ts
+++ src/api/nijisanji/nijisanji.ts
@@ -23,13 +23,30 @@ export interface NijiLiverMap {
 
 const proxyApiBase = "https://nijiapi-proxy.vercel.app/api";
 
-export async function getNijiLiverMap(isDev: boolean): Promise<NijiLiverMap> {
+async function _getNijiLiverMap(isDev: boolean): Promise<NijiLiverMap> {
   if (isDev) {
     return import("./sample3/livers.json").then((res) => res.default);
   }
 
   const url = new URL(`${proxyApiBase}/livers`);
   return fetch(url).then((res) => res.json());
+}
+
+export async function getNijiLiverMap(isDev: boolean): Promise<NijiLiverMap> {
+  const liverMap = await _getNijiLiverMap(isDev);
+
+  // Reduce image size
+  return Object.entries(liverMap).reduce(
+    (acc, [id, liver]) => {
+      const reducedImage = `${liver.image}&w=200&fm=webp`;
+      acc[id] = {
+        ...liver,
+        image: reducedImage,
+      };
+      return acc;
+    },
+    {} as Record<string, NijiLiver>,
+  );
 }
 
 export async function getNijiStreams(isDev: boolean): Promise<NijiStream[]> {
