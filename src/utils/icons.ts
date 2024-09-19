39488abdf48918d908ad79d366ce7379d544f0ee
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-19 23:28:29 +0000

    use defaultIcon

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,12 +1,10 @@
 import icons from "@/assets/icons.json";
 export const talentIcons: Record<string, string> = icons;
 
-const defaultIcon = "";
-
+const defaultIcon = "/defaultAccount.svg";
+const iconBase = "https://raw.githubusercontent.com/miyaoka/liver-streams/main/public/icons";
 export function getChannelIcon(name: string) {
   const path = talentIcons[name];
 
-  return path
-    ? `https://raw.githubusercontent.com/miyaoka/liver-streams/main/public/icons${path}`
-    : defaultIcon;
+  return path ? `${iconBase}${path}` : `${iconBase}${defaultIcon}`;
 }
