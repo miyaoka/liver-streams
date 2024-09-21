f2b4f3c4f0dfe250ef766ccf897322f729a565f5
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-21 08:52:01 +0000

    ライブが0件のときの色を変更

--- src/components/menu/LiveFilterButton.vue
+++ src/components/menu/LiveFilterButton.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useEventListStore } from "@/store/eventListStore";
 import { useStorageStore } from "@/store/storageStore";
 
 const storageStore = useStorageStore();
 const eventListStore = useEventListStore();
+const hasLiveEvents = computed(() => eventListStore.onLiveEventList.length > 0);
 </script>
 
 <template>
@@ -15,7 +17,7 @@ const eventListStore = useEventListStore();
     <p v-if="storageStore.isLiveOnly" class="bg-white text-red-500 font-bold">live</p>
     <i :class="`i-mdi-radio-tower w-8 h-8 ${storageStore.isLiveOnly ? 'bg-red-500' : ''}`" />
     <p
-      class="absolute bg-red-700 text-white text-xs rounded-xl px-1 -right-2 -top-2 min-w-5 h-5 flex items-center justify-center"
+      :class="`absolute ${hasLiveEvents ? 'bg-red-700' : 'bg-gray-700'} text-white text-xs rounded-xl px-1 -right-2 -top-2 min-w-5 h-5 flex items-center justify-center`"
     >
       {{ eventListStore.onLiveEventList.length }}
     </p>
