630f0e31d3d0762a1c15b809bb9b5921a4da9f17
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-23 08:48:58 +0000

    horizontal viewを削除

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 const VerticalView = () => import("@/views/VerticalView.vue");
-const HorizontalView = () => import("@/views/HorizontalView.vue");
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +9,7 @@ const router = createRouter({
       name: "vertical",
       component: VerticalView,
     },
-    {
-      path: "/h",
-      name: "horizontal",
-      component: HorizontalView,
-    },
+
     // fallback
     {
       path: "/:pathMatch(.*)*",
