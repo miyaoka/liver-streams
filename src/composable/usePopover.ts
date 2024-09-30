8b3dace93e708a2119cf3d5d678f90531042d799
Masaya Kazama <miyaokamiyao@gmail.com>
2024-09-30 09:22:08 +0000

    popoverの挙動修正

--- src/composable/usePopover.ts
+++ src/composable/usePopover.ts
@@ -14,10 +14,12 @@ export function usePopover(options: PopoverOptions = {}) {
   const isOpen = ref(false);
 
   let removePointerdownListener: (() => void) | null = null;
+  let removePointerupListener: (() => void) | null = null;
 
   useEventListener(popoverEl, "beforetoggle", async (evt: ToggleEvent) => {
     isOpen.value = evt.newState === "open";
     removePointerdownListener?.();
+    removePointerupListener?.();
 
     // 開閉時のハンドラ処理
     if (!isOpen.value) {
@@ -28,23 +30,20 @@ export function usePopover(options: PopoverOptions = {}) {
 
     // popoverの描画を待つ
     await new Promise((resolve) => requestAnimationFrame(resolve));
+    let popover: Element | null = null;
 
     // iOS safariではoutsideクリックで閉じないバグがあるので閉じる処理を追加
+    // pointerdown時に親を取得
     removePointerdownListener = useEventListener(document, "pointerdown", (e) => {
       const target = e.target as HTMLElement;
-      const popover = target.closest("[popover]");
+      popover = target.closest("[popover]");
+    });
 
+    // pointerup時にpopover外なら閉じる
+    removePointerupListener = useEventListener(document, "pointerup", () => {
       // popover内なら無視する（別のpopover内でも閉じないようにする）
       if (popover) return;
-
-      useEventListener(
-        document,
-        "pointerup",
-        () => {
-          hidePopover();
-        },
-        { once: true },
-      );
+      hidePopover();
     });
   });
 
