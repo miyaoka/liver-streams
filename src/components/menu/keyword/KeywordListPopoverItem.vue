6ed86cf03ad6279f68d9322bba87d3818befd8cb
Masaya Kazama <miyaokamiyao@gmail.com>
2024-10-02 02:19:54 +0000

    rename

--- src/components/menu/keyword/KeywordListPopoverItem.vue
+++ src/components/menu/keyword/KeywordListPopoverItem.vue
@@ -8,15 +8,15 @@ defineProps<{
 
 const searchStore = useSearchStore();
 
-function setSearchTerm(term: string) {
+function setSearchString(str: string) {
   // 空白を含むならダブルクォーテーションで囲む
-  const formattedTerm = term.includes(" ") ? `"${term}"` : term;
+  const formattedStr = str.includes(" ") ? `"${str}"` : str;
   // 同じものなら検索を解除
-  if (searchStore.searchString === formattedTerm) {
+  if (searchStore.searchString === formattedStr) {
     searchStore.setSearchString("");
     return;
   }
-  searchStore.setSearchString(formattedTerm);
+  searchStore.setSearchString(formattedStr);
 }
 </script>
 
@@ -26,7 +26,7 @@ function setSearchTerm(term: string) {
       v-for="item in itemList"
       :key="item.value"
       class="py-1 hover:bg-gray-100"
-      @click="setSearchTerm(item.value)"
+      @click="setSearchString(item.value)"
     >
       <div class="flex items-center gap-2 px-3 text-start">
         <p class="line-clamp-2 flex-1 [overflow-wrap:anywhere]">
