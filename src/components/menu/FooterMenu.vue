3269752707d106908e2e932b116031693dad954a
Masaya Kazama <miyaokamiyao@gmail.com>
2024-10-19 11:02:40 +0000

    複数選択ボタンを右側に移動

--- src/components/menu/FooterMenu.vue
+++ src/components/menu/FooterMenu.vue
@@ -9,9 +9,11 @@ import ScrollButton from "./ScrollButton.vue";
 import { getYouTubeVideoId } from "@/lib/youtube";
 import { useEventListStore } from "@/store/eventListStore";
 import { useFocusStore } from "@/store/focusStore";
+import { useSearchStore } from "@/store/searchStore";
 
 const focusStore = useFocusStore();
 const eventListStore = useEventListStore();
+const searchStore = useSearchStore();
 
 const multiviewPlayerUrl = "https://multiview-player.vercel.app/";
 
@@ -29,21 +31,39 @@ function openInMultiPlayer() {
   url.search = new URLSearchParams(vidList.map((vid) => ["v", vid])).toString();
   window.open(url.href, "_blank");
 }
+
+function selectAll() {
+  focusStore.isMultiSelectMode = true;
+  focusStore.multiSelectEventIdSet.clear();
+  eventListStore.filteredEventList.forEach((event) => {
+    focusStore.multiSelectEventIdSet.add(event.id);
+  });
+}
 </script>
 
 <template>
   <footer class="relative z-30">
     <FilteringButton />
 
-    <div class="fixed bottom-4 right-4 flex flex-col items-end gap-4">
+    <div class="fixed bottom-4 right-4 flex flex-col items-end gap-2">
       <div
         v-if="focusStore.isMultiSelectMode"
-        class="flex items-center justify-center gap-4 rounded-xl bg-green-700 px-4 py-2 text-white"
+        class="flex items-center justify-center gap-4 rounded-xl bg-green-700 px-4 py-2 text-white shadow-md"
       >
-        <div class="flex flex-col">
+        <div class="flex flex-col gap-1">
           <p class="text-lg font-bold">複数選択モード</p>
-          <p>選択した動画をマルチビュー画面で表示</p>
-          <p>（{{ focusStore.multiSelectEventIdSet.size }}個選択中）</p>
+          <p>
+            選択した動画をマルチビュー画面で表示<br />
+            （{{ focusStore.multiSelectEventIdSet.size }}個選択中）
+          </p>
+          <button
+            v-if="searchStore.hasQuery"
+            class="flex h-8 items-center justify-center gap-1 rounded-full bg-green-600/90 px-2"
+            @click="selectAll"
+          >
+            <i class="i-mdi-check-all size-6" />
+            全選択
+          </button>
         </div>
         <div>
           <button
@@ -56,13 +76,15 @@ function openInMultiPlayer() {
           </button>
         </div>
       </div>
-      <div class="flex flex-row items-end gap-2">
+      <div class="flex flex-col items-end gap-2">
         <MultiSelectButton />
-        <KeywordListButton />
-        <BookmarkedEventButton />
-        <AddedEventButton />
-        <LiveFilterButton />
-        <ScrollButton />
+        <div class="flex flex-row items-end gap-2">
+          <KeywordListButton />
+          <BookmarkedEventButton />
+          <AddedEventButton />
+          <LiveFilterButton />
+          <ScrollButton />
+        </div>
       </div>
     </div>
   </footer>
