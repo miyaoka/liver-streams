f0c0c340a1d18196c96e2c6e10c38aa41df57b51
Masaya Kazama <miyaokamiyao@gmail.com>
2024-11-08 14:14:42 +0000

    add group channel

--- src/assets/channel.ts
+++ src/assets/channel.ts
@@ -66,11 +66,18 @@ export const talents: ChannelNode[] = [
           },
           {
             name: "ReGLOSS",
-            children: ["火威青", "音乃瀬奏", "一条莉々華", "儒烏風亭らでん", "轟はじめ"],
+            children: ["ReGLOSS", "火威青", "音乃瀬奏", "一条莉々華", "儒烏風亭らでん", "轟はじめ"],
           },
           {
-            name: "FLOWGLOW",
-            children: ["響咲リオナ", "虎金妃笑虎", "水宮枢", "輪堂千速", "綺々羅々ヴィヴィ"],
+            name: "FLOW GLOW",
+            children: [
+              "FLOW GLOW",
+              "響咲リオナ",
+              "虎金妃笑虎",
+              "水宮枢",
+              "輪堂千速",
+              "綺々羅々ヴィヴィ",
+            ],
           },
         ],
       },
